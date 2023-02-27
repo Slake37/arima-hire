8d13d607edd413b2791c1c0fe121d46803ed06f1
Slake37 <moldovan.alin160@yahoo.co.uk>
2023-02-27 21:20:56 +0000

    margin changed on images

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -10,23 +10,23 @@ function Services() {
     <div className='mt-10'>
         <h2 className='text-[#F0A04B] font-bold text-4xl drop-shadow-lg m-6 md:text-5xl'>Services</h2>
         <div className='flex flex-wrap justify-center p-5'>
-            <div className='flex flex-col justify-center align-center text-center mx-5 drop-shadow-lg '>
+            <div className='flex flex-col justify-center align-center text-center mx-3 drop-shadow-lg '>
                 <img src={airplane} className='w-[150px] md:w-[200px]  rounded-md' alt="Airplane landing" />
                 <p>Airport transfer</p>
             </div>
-             <div className='flex flex-col justify-center align-center text-center mx-5 drop-shadow-lg'>
+             <div className='flex flex-col justify-center align-center text-center mx-3 drop-shadow-lg'>
                 <img src={dayOut} className='w-[150px] md:w-[200px]   rounded-md' alt="People having fun at day out"  />
                 <p>Day out</p>
             </div>
-            <div className='flex flex-col justify-center align-center text-center mx-5 drop-shadow-lg'>
+            <div className='flex flex-col justify-center align-center text-center mx-3 drop-shadow-lg'>
                 <img src={nightOut} className='w-[150px] md:w-[200px] rounded-md' alt="People having fun at night out" />
                 <p>Night out</p>
             </div>
-            <div className='flex flex-col justify-center align-center text-center mx-5 drop-shadow-lg'>
+            <div className='flex flex-col justify-center align-center text-center mx-3 drop-shadow-lg'>
                 <img src={wedding} className='w-[150px] md:w-[200px]  rounded-md' alt="People attending at a wedding"  />
                 <p>Wedding</p>
             </div>
-            <div className='flex flex-col justify-center align-center text-center mx-5 drop-shadow-lg'>
+            <div className='flex flex-col justify-center align-center text-center mx-3 drop-shadow-lg'>
                 <img src={event} className='w-[150px] md:w-[200px]  rounded-md' alt="People attending at an event" />
                 <p>Event</p>
             </div>

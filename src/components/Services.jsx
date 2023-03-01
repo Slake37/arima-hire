c7c19b1ba5723f1a0b40b31deaafdbe4d185c83f
Slake37 <moldovan.alin160@yahoo.co.uk>
2023-03-01 20:44:15 +0000

    new services cards

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,31 +4,47 @@ import dayOut from '../assets/day_out.jpg'
 import nightOut from '../assets/night-out.jpg'
 import wedding from '../assets/wedding.jpg'
 import event from '../assets/event.jpg'
+import {FiMoreHorizontal} from 'react-icons/fi'
 
 function Services() {
   return (
     <div className='mt-10'>
         <h2 className='text-[#F0A04B] font-bold text-4xl drop-shadow-lg m-6 md:text-5xl'>Services</h2>
-        <div className='flex flex-wrap justify-center p-5'>
-            <div className='flex flex-col justify-center align-center text-center mx-3 drop-shadow-lg '>
-                <img src={airplane} className='w-[150px] md:w-[200px]  rounded-md' alt="Airplane landing" />
-                <p>Airport transfer</p>
+        <div className='flex flex-col md:flex-row justify-center p-5'>
+            <div className='flex flex-col justify-center items-center text-center m-3 shadow-2xl bg-[#FEFBE9] p-3 rounded-xl'>
+                <img src={airplane} className='w-full md:w-[200px]  rounded-md' alt="Airplane landing" />
+                <div className='flex justify-between items-center mt-3 w-full'>
+                    <p className='font-medium text-lg'>Airport transfer</p>
+                    <FiMoreHorizontal className='text-3xl self-center border-2 p-1 border-black rounded-md cursor-pointer'/>
+                </div>
             </div>
-             <div className='flex flex-col justify-center align-center text-center mx-3 drop-shadow-lg'>
-                <img src={dayOut} className='w-[150px] md:w-[200px]   rounded-md' alt="People having fun at day out"  />
-                <p>Day out</p>
+             <div className='flex flex-col justify-center items-center text-center m-3 shadow-2xl bg-[#FEFBE9] p-3 rounded-xl'>
+                <img src={dayOut} className='w-full md:w-[200px]   rounded-md' alt="People having fun at day out"  />
+                <div className='flex justify-between items-center mt-3 w-full'>
+                    <p className='font-medium text-lg'>Day out</p>
+                    <FiMoreHorizontal className='text-3xl self-center border-2 p-1 border-black rounded-md cursor-pointer'/>
+                </div>
             </div>
-            <div className='flex flex-col justify-center align-center text-center mx-3 drop-shadow-lg'>
-                <img src={nightOut} className='w-[150px] md:w-[200px] rounded-md' alt="People having fun at night out" />
-                <p>Night out</p>
+            <div className='flex flex-col justify-center items-center text-center m-3 shadow-2xl bg-[#FEFBE9] p-3 rounded-xl'>
+                <img src={nightOut} className='w-full md:w-[200px] rounded-md' alt="People having fun at night out" />
+                <div className='flex justify-between items-center mt-3 w-full'>
+                    <p className='font-medium text-lg'>Night out</p>
+                    <FiMoreHorizontal className='text-3xl self-center border-2 p-1 border-black rounded-md cursor-pointer'/>
+                </div>
             </div>
-            <div className='flex flex-col justify-center align-center text-center mx-3 drop-shadow-lg'>
-                <img src={wedding} className='w-[150px] md:w-[200px]  rounded-md' alt="People attending at a wedding"  />
-                <p>Wedding</p>
+            <div className='flex flex-col justify-center items-center text-center m-3 shadow-2xl bg-[#FEFBE9] p-3 rounded-xl'>
+                <img src={wedding} className='w-[250px] md:w-[200px]  rounded-md' alt="People attending at a wedding"  />
+                <div className='flex justify-between items-center mt-3 w-full'>
+                    <p className='font-medium text-lg'>Wedding</p>
+                    <FiMoreHorizontal className='text-3xl self-center border-2 p-1 border-black rounded-md cursor-pointer'/>
+                </div>
             </div>
-            <div className='flex flex-col justify-center align-center text-center mx-3 drop-shadow-lg'>
-                <img src={event} className='w-[150px] md:w-[200px]  rounded-md' alt="People attending at an event" />
-                <p>Event</p>
+            <div className='flex flex-col justify-center items-center text-center m-3 shadow-2xl bg-[#FEFBE9] p-3 rounded-xl'>
+                <img src={event} className='w-[250px] md:w-[200px]  rounded-md' alt="People attending at an event" />
+                <div className='flex justify-between items-center mt-3 w-full'>
+                    <p className='font-medium text-lg'>Event</p>
+                    <FiMoreHorizontal className='text-3xl self-center border-2 p-1 border-black rounded-md cursor-pointer'/>
+                </div>
             </div>
         </div>
     </div>

f62ce61d352047638765aef5c2c3a70a22ce068f
Slake37 <moldovan.alin160@yahoo.co.uk>
2024-02-10 10:46:22 +0000

    reviews added and meet your driver update

--- src/components/MeetYourDriver.jsx
+++ src/components/MeetYourDriver.jsx
@@ -1,23 +1,52 @@
-import React from 'react'
-import profilePicture from '../assets/ionut_profile.jpg'
+import React from "react";
+import profilePicture from "../assets/ionut_profile.jpg";
+import vanPicture1 from "../assets/1.jpg";
+import vanPicture2 from "../assets/2.jpg";
+import vanPicture3 from "../assets/3.jpg";
+import vanPicture4 from "../assets/4.jpg";
 
 function MeetYourDriver() {
   return (
-    <div className='py-[7rem] px-2 md:px-[5rem] mx-auto w-fit '>
-        <h1 className='text-[#FEFBE9] font-Roboto-Condensed font-bold text-4xl md:text-5xl w-fit'>Hello</h1>
-        <div className='flex flex-col md:flex-row justify-center items-center mt-5 w-fit mx-auto'>
-            <img src={profilePicture} alt="Your driver" className='md:w-1/4  rounded-md' />
-            <p className='text-lg font-bold mt-5 mx-5 md:w-1/3'>
-                My name is Johnny. Actual is Ionut but translates into Johnny, so is easier for everyone. 
-                Originally I am from Transylvania ( you know, Dracula land), but lived in U.K. for more
-                than 10 years. At the minute I have 2 jobs. One job is NHS mental healt (doing that for the past 8 years)
-                and the other one I wouldn't consider it a job, it's my private hire business. I have all the trainings
-                and my DBS under close supervision by the NHS and Durham Council. If you require more information about me 
-                don't hesitate to ask me.
-            </p>
-        </div>
+    <div className="py-[7rem] px-2 md:px-[5rem] mx-auto w-fit ">
+      <h1 className="text-[#FEFBE9] font-Roboto-Condensed font-bold text-4xl md:text-5xl w-fit">
+        Hello
+      </h1>
+      <div className="flex flex-col md:flex-row justify-center items-center mt-5 w-fit mx-auto">
+        <img
+          src={profilePicture}
+          alt="Your driver"
+          className="md:w-1/4  rounded-md"
+        />
+        <p className="text-lg font-bold mt-5 mx-5 md:w-1/3">
+          My name is Johnny. Actual is Ionut but translates into Johnny, so is
+          easier for everyone. Originally I am from Transylvania ( you know,
+          Dracula land), but lived in U.K. for more than 10 years. At the minute
+          I have 2 jobs. One job is NHS mental healt (doing that for the past 8
+          years) and the other one I wouldn't consider it a job, it's my private
+          hire business. I have all the trainings and my DBS under close
+          supervision by the NHS and Durham Council. If you require more
+          information about me don't hesitate to ask me.
+        </p>
+      </div>
+      <div className="flex flex-wrap w-full gap-2 margin-auto justify-center items-center">
+        <img
+          src={vanPicture1}
+          alt="Arima Hire van picture"
+          className="object-contain rounded-md dropshadow-md max-w-[320px]"
+        />
+        <img
+          src={vanPicture2}
+          alt="Arima Hire van picture"
+          className="object-contain rounded-md dropshadow-md max-w-[320px]"
+        />
+        <img
+          src={vanPicture4}
+          alt="Arima Hire van picture"
+          className="object-contain rounded-md dropshadow-md max-w-[320px]"
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default MeetYourDriver
+export default MeetYourDriver;
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,75 +1,179 @@
-import React from 'react'
-import {BsFillStarFill} from 'react-icons/bs'
+import React from "react";
+import { BsFillStarFill } from "react-icons/bs";
 
 function Reviews() {
   return (
-    <div className='py-20 px-5 md:w-1/2 mx-auto'>
-        <h1 className='text-[#E1EEDD] font-bold text-2xl mb-5 md:text-3xl'>What our passengers have to say</h1>
-        <div className='border-2 p-3 rounded-md drop-shadow-2xl my-3'>
-            <h3 className='text-black text-xl font-medium underline'>Gemma Heppell</h3>
-            <p className='text-black font-normal'>I have used Arima Hire on a number of occasion now and every time they have arrived on time. I have always felt safe as I mainly travel alone. The driver is friendly & helpful and always went that extra mile.
-               The vehicle is spacious and clean.
-               Competitive prices.</p>
-               <div className='flex mt-3 justify-end'>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-               </div>
+    <div className="py-20 px-5 md:w-1/2 mx-auto">
+      <h1 className="text-[#E1EEDD] font-bold text-2xl mb-5 md:text-3xl">
+        What our passengers have to say
+      </h1>
+      <div className="border-2 p-3 rounded-md drop-shadow-2xl my-3">
+        <h3 className="text-black text-xl font-medium underline">
+          Gemma Heppell
+        </h3>
+        <p className="text-black font-normal">
+          I have used Arima Hire on a number of occasion now and every time they
+          have arrived on time. I have always felt safe as I mainly travel
+          alone. The driver is friendly & helpful and always went that extra
+          mile. The vehicle is spacious and clean. Competitive prices.
+        </p>
+        <div className="flex mt-3 justify-end">
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
         </div>
-        <div className='border-2 p-3 rounded-md drop-shadow-2xl my-3'>
-            <h3 className='text-black text-xl font-medium underline'>Shirley Oliver</h3>
-            <p className='text-black font-normal'>Very professional and helpful. Reasonably priced as well.
-            Effective communication before and after trip.
-            Will definitely use again.</p>
-             <div className='flex mt-3 justify-end'>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-               </div>
+      </div>
+      <div className="border-2 p-3 rounded-md drop-shadow-2xl my-3">
+        <h3 className="text-black text-xl font-medium underline">
+          Shirley Oliver
+        </h3>
+        <p className="text-black font-normal">
+          Very professional and helpful. Reasonably priced as well. Effective
+          communication before and after trip. Will definitely use again.
+        </p>
+        <div className="flex mt-3 justify-end">
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
         </div>
-        <div className='border-2 p-3 rounded-md drop-shadow-2xl my-3 '>
-            <h3 className='text-black text-xl font-medium underline'>Helen Viard</h3>
-            <p className='text-black font-normal'>Arrived on time.
-                Polite and friendly driver. Clean cab.
-                Reasonably priced.
-                Will definitely use again</p>
-                 <div className='flex mt-3 justify-end'>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-               </div>
+      </div>
+      <div className="border-2 p-3 rounded-md drop-shadow-2xl my-3 ">
+        <h3 className="text-black text-xl font-medium underline">
+          Helen Viard
+        </h3>
+        <p className="text-black font-normal">
+          Arrived on time. Polite and friendly driver. Clean cab. Reasonably
+          priced. Will definitely use again
+        </p>
+        <div className="flex mt-3 justify-end">
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
         </div>
-        <div className='border-2 p-3 rounded-md drop-shadow-2xl my-3 '>
-            <h3 className='text-black text-xl font-medium underline'>Abba Mustafa</h3>
-            <p className='text-black font-normal'>Great service, very reliable and always available on SOS. I will definitely patronise again soon</p>
-             <div className='flex mt-3 justify-end'>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-               </div>
+      </div>
+      <div className="border-2 p-3 rounded-md drop-shadow-2xl my-3 ">
+        <h3 className="text-black text-xl font-medium underline">
+          Abba Mustafa
+        </h3>
+        <p className="text-black font-normal">
+          Great service, very reliable and always available on SOS. I will
+          definitely patronise again soon
+        </p>
+        <div className="flex mt-3 justify-end">
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
         </div>
-        <div className='border-2 p-3 rounded-md drop-shadow-2xl my-3 '>
-            <h3 className='text-black text-xl font-medium underline'>Pop Ovidiu</h3>
-            <p className='text-black font-normal'>Spacious vehicle, friendly driver and competitive prices. In a few words, great choice.</p>
-             <div className='flex mt-3 justify-end'>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-                    <BsFillStarFill className='text-[#F0A04B] mx-1'/>
-               </div>
+      </div>
+      <div className="border-2 p-3 rounded-md drop-shadow-2xl my-3 ">
+        <h3 className="text-black text-xl font-medium underline">Pop Ovidiu</h3>
+        <p className="text-black font-normal">
+          Spacious vehicle, friendly driver and competitive prices. In a few
+          words, great choice.
+        </p>
+        <div className="flex mt-3 justify-end">
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
         </div>
-        
+      </div>
+      <div className="border-2 p-3 rounded-md drop-shadow-2xl my-3 ">
+        <h3 className="text-black text-xl font-medium underline">
+          Jonathan Anderson
+        </h3>
+        <p className="text-black font-normal">
+          We used Jonny from CLS To NCL. 5* service at a great price. Had a
+          message in the week before the flight to reconfirm the booking and a
+          message when the plane landed to tell me he was there! We’ve already
+          booked for our next trip!
+        </p>
+        <div className="flex mt-3 justify-end">
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+        </div>
+      </div>
+      <div className="border-2 p-3 rounded-md drop-shadow-2xl my-3 ">
+        <h3 className="text-black text-xl font-medium underline">
+          Kayden Horsley
+        </h3>
+        <p className="text-black font-normal">
+          Absolutely amazing service on time for both pick ups from home and
+          airport would 1000% recommend Quality service for a quality price 👍
+        </p>
+        <div className="flex mt-3 justify-end">
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+        </div>
+      </div>
+      <div className="border-2 p-3 rounded-md drop-shadow-2xl my-3 ">
+        <h3 className="text-black text-xl font-medium underline">
+          Brian Charles
+        </h3>
+        <p className="text-black font-normal">
+          Experience from first message to last has been brilliant.would
+          definitely recommend this company, fair price and very comfortable
+          travel... will definitely use again
+        </p>
+        <div className="flex mt-3 justify-end">
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+        </div>
+      </div>
+      <div className="border-2 p-3 rounded-md drop-shadow-2xl my-3 ">
+        <h3 className="text-black text-xl font-medium underline">
+          Brigid Holder
+        </h3>
+        <p className="text-black font-normal">
+          Have used Arima several times and cannot fault. Always early for pick
+          up and, no matter how early it is he is always happy. The service is
+          great, the driving is good and there's plenty of room for people and
+          luggage. If possible we'll always use Arima.
+        </p>
+        <div className="flex mt-3 justify-end">
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+        </div>
+      </div>
+      <div className="border-2 p-3 rounded-md drop-shadow-2xl my-3 ">
+        <h3 className="text-black text-xl font-medium underline">
+          Lynette Hinnigan
+        </h3>
+        <p className="text-black font-normal">
+          Spot on service, no complaints at all. Affordable prices, good
+          communication. Beautifully clean and tidy taxi.
+        </p>
+        <div className="flex mt-3 justify-end">
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+          <BsFillStarFill className="text-[#F0A04B] mx-1" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Reviews
+export default Reviews;
